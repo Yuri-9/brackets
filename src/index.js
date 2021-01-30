14d7cefde1a64da4b080565c13969a4e144aa2cc
Yuri-9 <vashkevichyp@gmail.com>
2021-01-30 03:46:51 +0300

    feat: add solution brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let bufferArray = [];
+    strArray = str.split("");
+
+    let isOpenAndCloseBrackets = (bufArr) => {
+        return bracketsConfig.some((item) => {
+            return (
+                item[0] == bufArr[bufArr.length - 2] &&
+                item[1] == bufArr[bufArr.length - 1]
+            );
+        });
+    };
+
+    strArray.forEach((elem) => {
+        bufferArray.push(elem);
+        if (isOpenAndCloseBrackets(bufferArray)) {
+            bufferArray.splice(-2, 2);
+        }
+    });
+
+    return bufferArray.length > 0 ? false : true;
+};
